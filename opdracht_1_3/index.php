736c115cfc17f6e630c426578312f8b96552503a
iNF3CT0R <103606438+iNF3CT0R@users.noreply.github.com>
2025-02-18 12:10:16 +0100

    opdracht 1.3

---
+++ opdracht_1_3/index.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<h1>WAT IS GITHUB??? kort uigelegd</h1>
+		<?php
+		echo "<p>GitHub is een zeer handige platform waar je jouw codes kan opslaan en organiseren in je eigen profiel.</p>
+		<p>Je kan ook profielen van anderen of organisaties bezoeken en hun codes inzien.</p>
+		<p>Het platform biedt heel veel opties aan in een fijne omgeving en je profiel te personaliseren.</p>
+		<p>Verder kan je ook goed samenwerken en communiceren met anderen of groepen/organisaties vormen en codes delen.</p>
+		<p>In conclusie is GitHub zeker een platform dat elk programmeur ooit moet proberen.</p>"
+		?>
+	</body>
+</html>
